d22be7962bde52199916fd6f00ea8696098ecd8a
dhruvsoni461 <dhruvkumarsoni461@gmail.com>
2025-08-12 11:30:13 +0530

    comparisions done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score =33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let variable = "33"
+
+// console.log(typeof variable);
+// console.log(typeof (variable));
+
+let variable_2 ="33abc"
+
+let value= String(variable_2)
+
+// console.log(typeof value);
+// console.log(value);
+
+let variable_3 = 33
+
+let stringType= String(variable_3)
+
+// console.log(stringType);
+// console.log(typeof stringType);
+
+
+// let variable_4= null
+let variable_4 ;
+
+let nullType= String(variable_4)
+
+console.log(nullType);
+console.log(typeof nullType);
+
+
+
