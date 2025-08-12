9dae1ffc29ae74002f555da7a7988454195c5694
dhruvsoni461 <dhruvkumarsoni461@gmail.com>
2025-08-12 08:21:24 +0530

    variables and data types done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId= "12345"
+var accountEmail= "dhruv@gmail.com"
+let accountPasword= "54321"
+accountCity= "bhopal"
+let accountState;
+
+console.log([accountId, accountEmail, accountPasword, accountCity, accountState])
+
+// prefer not to use var to functional scope problem
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+let name= "Dhruv"
+let age= "18"
+let isUserLoggedIn= true
+let state= null // in the place of just ";" to avoid 'undefined' output
+
+
+console.log([name, age, isUserLoggedIn]);
+console.log(state);
+console.log(typeof null);
+console.log(typeof isUserLoggedIn);
+console.log(typeof age);
+
+
+
+
+
+
+
+// number, bigInt, boolean, string
+//nul=> stand alone value . null is a type 
+//undefined=> 
+//symbol
+//
+
