ad4fbdee036fb5846db598aa5d4f8d7d470b5643
dhruvsoni461 <dhruvkumarsoni461@gmail.com>
2025-08-20 21:28:17 +0530

    mini projects on events done

---
+++ 08_events/events.js
@@ -0,0 +1,5 @@
+// here we have to note down that js is a synchrounous and single threaded languageli
+// //execute one line of a code at a time      1).CALL STACK  2).MEMORY HEAP
+// there are 2 types of code in js
+// 1). blocking code- blockes the flow of program , read file sync
+// 2). non-blocking code- dosen't block execution, read file async
