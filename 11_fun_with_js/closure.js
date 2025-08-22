dd22565eb1497f104c15f298271f025c265f7a2e
dhruvsoni461 <dhruvkumarsoni461@gmail.com>
2025-08-22 21:19:38 +0530

    closures done

---
+++ 11_fun_with_js/closure.js
@@ -0,0 +1,25 @@
+// function init() {
+    //     let name = "Mozilla"; 
+    //     function displayName() {
+    //         console.log(name);         // we cant access it because of closure
+    //     }
+    //     displayName();
+    // }
+    // init();
+
+
+    //------------------------------------------------------------------------------------------------//
+
+// function makeFunc() {
+//         const name = "Mozilla";
+//         function displayName() {
+//             console.log(name);            // now we can access bcz of lexical scoping  
+//         }
+//         return displayName;
+//     }
+
+//     const myFunc = makeFunc();
+//     myFunc();
+
+
+    //--------------------------------------------------------------------------------------------------//
